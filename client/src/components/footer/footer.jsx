bcb9375c65253bd5bcf180100cbe21ece837e9af
Derek Cabrera <cabrera.derek.adriel@gmail.com>
2022-09-10 23:15:52 -0300

    estilos en el footer

--- client/src/components/footer/footer.jsx
+++ client/src/components/footer/footer.jsx
@@ -5,28 +5,27 @@ const Footer = () => {
 
     return (
         <Box className={style.contentFooter}>
-           <div>
+           <div className={style.contentLogo}>
             <h1>Logo</h1>
            </div>
-           <div >
-            <div className={style.contentICons}>
-                <ul>
-                     <li>Home</li>
-                     <li>Nosotros</li>
-                      <li>Servicios</li>
-                       <li>Proyectos</li>
-                       <li>Contacto</li>
+           <div className={style.contentListado}>
+            <div className={style.contentUl}>
+                <ul className={style.Ul}>
+                     <li className={style.li}>Home</li>
+                     <li className={style.li}>Nosotros</li>
+                      <li className={style.li}>Servicios</li>
+                       <li className={style.li}>Proyectos</li>
+                       <li className={style.li}>Contacto</li>
             </ul>
             </div>
-           
-            <h4> 2022 © Todos los derechos reservados a FreeDevs</h4>
+            <h4 className={style.title}> 2022 © Todos los derechos reservados a FreeDevs</h4>
            </div>
-           <div>
-            <div>
-                <FaInstagram/>
-                <FaLinkedin/>
-                <FaEnvelope/>
-                <FaWhatsapp/>
+           <div className={style.contentIConsEmail}>
+            <div className={style.contentICons}>
+                <FaInstagram className={style.icon}/>
+                <FaLinkedin className={style.icon}/>
+                <FaEnvelope className={style.icon}/>
+                <FaWhatsapp className={style.icon}/>
             </div>
            <div>
              <h4>Suport FreeDevs@gmail.com</h4>
