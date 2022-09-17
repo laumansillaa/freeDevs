6f84c25796f70ff11cd47bb92ee94285e727c28e
Derek Cabrera <cabrera.derek.adriel@gmail.com>
2022-09-17 13:45:04 -0300

    se cambio el enrutado

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,8 @@ import {Routes, Route} from 'react-router-dom'
 import Home from './components/home/home'
 import Nav from './components/nav/nav';
 import './App.css'
-
+import Servicie from './components/Servicie/Servicie';
+import Footer from './components/footer/footer'
 
 
 function App() {
@@ -11,9 +12,9 @@ function App() {
 
     <div className="App">
         <Nav/>
-        <Routes>
-          <Route path='/' element={<Home/>}/>
-        </Routes>
+          <Home/>
+          <Servicie/>
+          <Footer/>
     </div>
   )
 }
--- client/src/components/home/home.jsx
+++ client/src/components/home/home.jsx
@@ -50,6 +50,7 @@ const Home = () => {
                         </Box>
                     </Box>
                 </Box>
+            
             </Box>
 
 
