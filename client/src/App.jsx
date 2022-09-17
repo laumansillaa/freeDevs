fb3128bd7a028bcac4c92c44ce3d0926e4886920
Derek Cabrera <cabrera.derek.adriel@gmail.com>
2022-09-17 00:24:30 -0300

    Actualizacion 2.0

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,9 +2,8 @@ import {Routes, Route} from 'react-router-dom'
 import Home from './components/home/home'
 import Nav from './components/nav/nav';
 import './App.css'
-import BackgroundParticles from './components/particles';
-import Servicie from './components/Servicie/Servicie';
-// import Footer from './components/footer/footer';
+
+
 
 function App() {
 
@@ -12,10 +11,8 @@ function App() {
 
     <div className="App">
         <Nav/>
-          {/* <BackgroundParticles/> */}
         <Routes>
           <Route path='/' element={<Home/>}/>
-          <Route path='/servicie' element={<Servicie/>}/>
         </Routes>
     </div>
   )
--- client/src/components/particles/index.jsx
+++ client/src/components/particles/index.jsx
@@ -1,13 +1,13 @@
 import Particles from "react-particles";
 import { loadFull } from "tsparticles";
 import { useCallback } from "react";
+import styles from './styles/index.module.css'
+
 
 const BackgroundParticles = () => {
   const particlesInit = useCallback(async (engine) => {
     console.log(engine);
-    // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
-    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-    // starting from v2 you can add only the features you need reducing the bundle size
+
     await loadFull(engine);
   }, []);
 
@@ -18,6 +18,7 @@ const BackgroundParticles = () => {
   return (
     <>
       <Particles
+        className={styles.container}
         id='tsparticles'
         init={particlesInit}
         options={{
