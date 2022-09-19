72670135dbb85185724d805695fa8b059596dcd9
Derek Cabrera <cabrera.derek.adriel@gmail.com>
2022-09-19 20:57:20 -0300

    algo

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,8 @@
 import {Routes, Route} from 'react-router-dom'
 import Home from './components/home/Home'
 import Nav from './components/nav/nav';
-import Service from './components/Servicie/Servicie'
+import Servicie from './components/Servicie/Servicie'
 import './App.css'
-import Servicie from './components/Servicie/Servicie';
 import Contact from './components/contact/Contact'
 import Footer from './components/footer/footer'
 
