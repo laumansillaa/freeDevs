27d7e56e1f1e8e760c95bbf14d4a930cb51f2aee
Lautaro Mansilla <85461971+laumansillaa@users.noreply.github.com>
2022-09-22 11:29:51 -0300

    correcion en Nav

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,8 +14,8 @@ function App() {
     <div className="App">
         <Nav/>
           <Home/>
-          <Servicie/>
-          <Footer/>
+          {/* <Servicie/>
+          <Footer/> */}
     </div>
   )
 }
--- client/src/components/nav/nav.jsx
+++ client/src/components/nav/nav.jsx
@@ -1,4 +1,5 @@
 import {Box, Typography} from '@mui/material'
+import { Link } from 'react-router-dom'
 import freeDev from '../../assets/svg/freeDevs.svg'
 import style from './styles/index.module.css'
 import menu from '../../assets/svg/menu.svg'
@@ -6,6 +7,11 @@ import lampara from '../../assets/lampara.png'
 
 
 const Nav = () => {
+
+    const clickChecked = (e) => {
+        // e.preventDefault()
+        const gsm = document.getElementById('menu').click()
+    }
 
 
     return (
@@ -19,10 +25,10 @@ const Nav = () => {
                 </label>
                 <input type='checkbox' id='menu' className={style.navInput}></input>
                 <Box className={style.contItemsNav}>
-                    <a href='/a' className={style.navItem}>Nosotros</a>
-                    <a href='/a' className={style.navItem}>Servicios</a>
-                    <a href='/a' className={style.navItem}>Proyectos</a>
-                    <a href='/a' className={style.navItem}>Contacto</a>
+                    <Link to='/a' className={style.navItem} onClick={clickChecked }>Nosotros</Link>
+                    <Link to='#Service' className={style.navItem} onClick={clickChecked}>Servicios</Link>
+                    <Link to='/a' className={style.navItem} onClick={clickChecked}>Proyectos</Link>
+                    <Link to='#Footer' className={style.navItem} onClick={clickChecked}>Contacto</Link>
                 </Box>
                 <Box className={style.contLampara}>
                     <img src={lampara} alt='light-png' className={style.lampara}/>
