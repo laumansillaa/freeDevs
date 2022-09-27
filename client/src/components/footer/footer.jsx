a39ca04b220e1fe82322784423cd28c561903bcd
Derek Cabrera <cabrera.derek.adriel@gmail.com>
2022-09-27 18:28:58 -0300

    footer listo

--- client/src/components/footer/footer.jsx
+++ client/src/components/footer/footer.jsx
@@ -9,40 +9,8 @@ const Footer = () => {
            <div className={style.contentLogo}>
             <h1>Logo</h1>
            </div>
-           <div className={style.contentListado}>
-            <div className={style.contentUl}>
-                <ul className={style.Ul}>
-
-                 <li value="home" className={style.li}>
-                     <Link to="home" spy={true} smooth={true} offset={-100} duration={200} >
-                         Home
-                      </Link>
-                   </li>
-             
-                
-               <li value="about" className={style.li}>
-                  <Link to="about" spy={true} smooth={true} offset={-100} duration={200} >
-                      Nosotros
-                </Link>
-              </li>
-               
-              <li value='servicio' className={style.li}>
-                 <Link to="servicio" spy={true} smooth={true} offset={-100} duration={200} >
-                   Servicios
-                 </Link>
-               </li>
-              
-                
-                 <li value='contact' className={style.li}>
-                    <Link to="contact" spy={true} smooth={true} offset={-100} duration={200}  >
-                      Contacto
-                    </Link>
-                 </li>
-               
-            </ul>
-            </div>
-            <h4 className={style.title}> 2022 © Todos los derechos reservados a FreeDevs</h4>
-           </div>
+       
+            
            <div className={style.contentIConsEmail}>
               <div className={style.contentICons}>
                 <FaInstagram className={style.icon}/>
@@ -55,6 +23,10 @@ const Footer = () => {
            </div>
            
            </div>
+
+           <div>
+            <h4 className={style.title}> 2022 © Todos los derechos reservados a FreeDevs</h4>
+          </div>
         </Box>
     )
 }
